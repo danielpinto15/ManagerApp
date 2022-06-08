417874b1a58d0b78d5123158c965fce589cb6e18
Dani <danielpinto15@gmail.com>
2022-06-08 11:13:09 +0100

    1st commit

---
+++ src/test/java/com/managerApp/managerApp/ManagerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.managerApp.managerApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
